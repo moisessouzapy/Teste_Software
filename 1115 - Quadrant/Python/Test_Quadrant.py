747f69d230e9a9269a23193d30bd471152cc3ebc
Moisés Souza <86939796+moisessouzapy@users.noreply.github.com>
2022-10-09 18:42:02 -0300

    Add files via upload

---
+++ 1115 - Quadrant/Python/Test_Quadrant.py
@@ -0,0 +1,55 @@
+from Coordinate import Coordinate
+from Quadrant import Quadrant
+
+
+def test_should_get_quadrant_coordinate():
+    # Arrange / Setup
+    a = 10
+    b = 20
+    coordinates = Coordinate(a, b)
+    quadrant = Quadrant(coordinates)
+
+    # Act / Action
+    result = quadrant.get_quadrant_coordinate()
+
+    # Assert
+    assert result == "First"
+
+def teste_second():
+    # Arrange / Setup
+    a = -20
+    b = 20
+    coordinates = Coordinate(a, b)
+    quadrant = Quadrant(coordinates)
+
+    # Act / Action
+    result = quadrant.get_quadrant_coordinate()
+
+    # Assert
+    assert result == "Second"
+
+def teste_third():
+    # Arrange / Setup
+    a = -20
+    b = -20
+    coordinates = Coordinate(a, b)
+    quadrant = Quadrant(coordinates)
+
+    # Act / Action
+    result = quadrant.get_quadrant_coordinate()
+
+    # Assert
+    assert result == "Third"
+
+def teste_fourth():
+    # Arrange / Setup
+    a = 20
+    b = -20
+    coordinates = Coordinate(a, b)
+    quadrant = Quadrant(coordinates)
+
+    # Act / Action
+    result = quadrant.get_quadrant_coordinate()
+
+    # Assert
+    assert result == "Fourth"
